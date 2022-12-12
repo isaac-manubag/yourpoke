1572a68cac4ac83cd5017a2c0edfa9579a9e0141
Isaac Manubag <isaac_manubag@yahoo.com>
2022-12-12 14:14:25 +0800

    your poke init

---
+++ laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Auth;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): JsonResponse
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function register(Request $request): JsonResponse
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => \Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        event(new Registered($user));
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+//    public function setPassword(Request $request): JsonResponse
+//    {
+//        $validatedData = $request->validate([
+//            'email' => 'required|string|email|max:255|exists:users',
+//            'password' => 'required|string|min:8',
+//        ]);
+//
+//        $user = User::query()->where('email', $validatedData['email'])->firstOrFail();
+//        $user->password = \Hash::make($validatedData['password']);
+//        $user->email_verified_at = Carbon::now();
+//        $user->save();
+//
+//        $token = $user->createToken('auth_token')->plainTextToken;
+//
+//        return response()->json([
+//            'access_token' => $token,
+//            'token_type' => 'Bearer',
+//        ]);
+//    }
+
+
+}
---
+++ laravel/routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\VerifyEmailController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/email/verify/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+    ->middleware(['signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
+Route::post('/email/verify/resend', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth:sanctum', 'throttle:6,1'])->name('verification.send');
